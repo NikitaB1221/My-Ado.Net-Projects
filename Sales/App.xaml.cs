abde36424ff2e9bfb9a9388cd7465f0f9013148e
NikitaB1221 <44338356+NikitaB1221@users.noreply.github.com>
2023-02-03 00:23:58 +0200

    CRUD 1

---
+++ Sales/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sales
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // строка подключения - берется из свойств БД (Server Explorer)
+        public const String ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\ШАГ\Ado.NET\ADO111\Sales\Sales111.mdf;Integrated Security=True";
+    }
+}
